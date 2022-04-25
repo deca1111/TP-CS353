7f23c61d47d2580394ae9ce1f8a342c6e4c569c2
Léo vltt <81568081+deca1111@users.noreply.github.com>
2022-04-25 19:19:49 +0200

    tp3 avancement

--- TP3/main.c
+++ TP3/main.c
@@ -9,13 +9,15 @@
 int main()
 {
 		clrscr();
-		
+
     printf("****** Gestionnaire de magasin ******\n");
     init();
 		insertItem(10001,"SUCRE - 500G",1.5);
 		insertItem(10002,"SUCRE - 1KG",2.2);
 		insertItem(10003,"SUCRE - 10KG",20);
 		insertItem(583,"POIVRE - VRAC 100G",1.27);
+    dumpItems();
+		suppressItem(583);
     dumpItems();
     printf("\n\n=======Gestionnaire de magasin ======\n");
 
